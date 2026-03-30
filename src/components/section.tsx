2d7ce9eccb008e69230e0877c7d894b178b66e63
eduardoqsilva <eduadsilva@outlook.com>
2026-03-30 00:11:38 -0300

    update

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,33 +1,67 @@
 import type { ComponentProps } from "react"
-import {twMerge} from "tailwind-merge"
+import { twMerge } from "tailwind-merge"
 
 interface SectionRootProps extends ComponentProps<"div"> {}
 
-function SectionRoot({className, ...props}:SectionRootProps) {
-  return <div className={twMerge("bg-navy-800 rounded-xl border-[0.5px] border-navy-500 pt-3 flex flex-col gap-1", className)} {...props}></div>
+function SectionRoot({ className, ...props }: SectionRootProps) {
+  return (
+    <div
+      className={twMerge(
+        "bg-navy-800 rounded-xl border-[0.5px] border-navy-500 pt-3 flex flex-col gap-1",
+        className,
+      )}
+      {...props}
+    ></div>
+  )
 }
 
 interface SectionHeaderProps extends ComponentProps<"div"> {}
 
-function SectionHeader({className, ...props}:SectionHeaderProps) {
-  return <div className={twMerge("flex items-center justify-between px-3", className)} {...props}></div>
+function SectionHeader({ className, ...props }: SectionHeaderProps) {
+  return (
+    <div
+      className={twMerge("flex items-center justify-between px-3", className)}
+      {...props}
+    ></div>
+  )
 }
 
 interface SectionTitleProps extends ComponentProps<"div"> {}
 
-function SectionTitle({className, ...props}:SectionTitleProps) {
-  return <div className={twMerge("bg-navy-700 rounded-lg px-3 py-1.5 flex items-center gap-2 text-xs", className)} {...props}></div>
+function SectionTitle({ className, ...props }: SectionTitleProps) {
+  return (
+    <div
+      className={twMerge(
+        "bg-navy-700 rounded-lg px-3 py-1.5 flex items-center gap-2 text-xs",
+        className,
+      )}
+      {...props}
+    ></div>
+  )
 }
 
 interface SectionIssueCountProps extends ComponentProps<"div"> {}
-function SectionIssueCount({className, ...props}:SectionIssueCountProps) {
-  return <div className={twMerge("text-xs text-navy-200", className)} {...props}></div>
+function SectionIssueCount({ className, ...props }: SectionIssueCountProps) {
+  return (
+    <div
+      className={twMerge("text-xs text-navy-200", className)}
+      {...props}
+    ></div>
+  )
 }
 
 interface SectionContentProps extends ComponentProps<"div"> {}
 
-function SectionContent({className, ...props}:SectionContentProps) {
-  return <div className={twMerge("flex flex-col gap-2.5 overflow-y-scroll p-3", className)} {...props}></div>
+function SectionContent({ className, ...props }: SectionContentProps) {
+  return (
+    <div
+      className={twMerge(
+        "flex flex-col gap-2.5 overflow-y-auto p-3",
+        className,
+      )}
+      {...props}
+    ></div>
+  )
 }
 
 export const Section = {
